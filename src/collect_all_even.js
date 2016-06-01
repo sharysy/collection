b40b8bec00ffc4a6b710662d59549a3ac703e6e6
xiongchaohu <1732139715@qq.com>
2016-06-01 17:16:10 +0800

    'change'

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function collect_all_even(collection) {
-  //在这里写入代码
-  	var temp=[];
-  		for(var i=0;i<collection.length;i++)
-  		{
+  //在这里写入代码	
+  var temp=[];
+  for(var i=0;i<collection.length;i++)
+  {
 
-  			if(collection[i]%2==0)
-  			{
-  				temp.push(collection[i]);
-  			}
-  		}
-  		return temp;
+  	if(collection[i]%2==0)
+  	{
+  		temp.push(collection[i]);
+  	}
+  }
+  return temp;
 }
 
 module.exports = collect_all_even;
